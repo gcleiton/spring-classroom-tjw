0bcde5caf62ef512bef088cf4ac7fc3fc72af0a7
Gabriel Cleiton <gabriel.n@lyncas.net>
2023-05-30 18:29:02 -0300

    setup project

---
+++ src/main/java/br/edu/ifce/springclassroomapi/SpringClassroomApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.springclassroomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClassroomApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClassroomApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifce/springclassroomapi/SpringClassroomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.springclassroomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClassroomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
